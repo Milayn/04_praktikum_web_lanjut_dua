7bc5b14578de0055e1454c0c511fd5e106a78ca5
Milayn <millayunita07@gmail.com>
2022-03-25 01:49:12 +0700

    Mengambil data pegawai ID > 10

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -11,10 +11,8 @@ class PegawaiController extends Controller
 {
     public function index()
     {
-    	// mengambil data pegawai yang bernama Raditya Maulana
-        // $pegawai = Pegawai::where('nama', 'Raditya Maulana')->get();
-        //cara 2
-        $pegawai = Pegawai::where('nama', '=' , 'Raditya Maulana')->get();
+    	/// mengambil data pegawai yang id nya lebih besar dari 10
+            $pegawai = Pegawai::where('id', '>' , 10)->get();
     	// mengirim data pegawai ke view pegawai
     	return view('pegawai', ['pegawai' => $pegawai]);
     }
