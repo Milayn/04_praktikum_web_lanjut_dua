32c3231d482a4a278dcc4ed7d8c271bd212399f8
Milayn <millayunita07@gmail.com>
2022-03-25 01:32:48 +0700

    Menggunakan fungsi find()

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -11,8 +11,8 @@ class PegawaiController extends Controller
 {
     public function index()
     {
-    	// mengambil data pegawai
-    	$pegawai = Pegawai::first();
+    	// mengambil data pegawai yang id nya 1
+    	$pegawai = Pegawai::find(1);
  
     	// mengirim data pegawai ke view pegawai
     	return view('pegawai', ['pegawai' => $pegawai]);
