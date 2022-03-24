2ec8d3ce1d2fbcfa44acef6d8bfbc1d665dbb6ff
Milayn <millayunita07@gmail.com>
2022-03-25 01:45:16 +0700

    Menggunakan fungsi where(2)

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -12,7 +12,9 @@ class PegawaiController extends Controller
     public function index()
     {
     	// mengambil data pegawai yang bernama Raditya Maulana
-        $pegawai = Pegawai::where('nama', 'Raditya Maulana')->get();
+        // $pegawai = Pegawai::where('nama', 'Raditya Maulana')->get();
+        //cara 2
+        $pegawai = Pegawai::where('nama', '=' , 'Raditya Maulana')->get();
     	// mengirim data pegawai ke view pegawai
     	return view('pegawai', ['pegawai' => $pegawai]);
     }
